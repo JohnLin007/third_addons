57b18e496a43a4b1325871bb07b2bc6227753a24
tianfusheng <979581151@qq.com>
2022-12-21 17:49:13 +0800

    update

--- tfs_form_update_buttons/static/src/js/tfs_form_update_buttons.js
+++ tfs_form_update_buttons/static/src/js/tfs_form_update_buttons.js
@@ -10,7 +10,7 @@ odoo.define('tfs_form_update_buttons.update_buttons', function (require) {
             return this._super.apply(this, arguments).then(res => {
                 this._updateControlPanel();
                 if (this.mode == 'edit'){
-                    this.mode = 'readonly';
+                    this._setMode('readonly')
                     this.updateButtons()
                 }
             })
